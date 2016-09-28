7814a269204a95593b32bb6634ecd16157c60500
Gregory Shuford <bearshuford@me.com>
2016-09-28 14:45:34 -0400

    completed question 4

--- refactor.js
+++ refactor.js
@@ -112,6 +112,10 @@ var reverseStr = function(str) {
 
 // Put your answer below -------------------------
 
+   var reverseStr = function(str) {
+     return str.split("").reverse().join("");
+   };
+   console.log(reverseStr('bear'));
 
 // -----------------------------------------------
 
